c93499860d6bdd1bb01e918c2975df353191c648
Diego Duarte <duartqx@gmail.com>
2022-05-03 19:50:03 -0300

    small tweaks and added requirements

--- read_pdf.py
+++ read_pdf.py
@@ -3,18 +3,7 @@ import pdfplumber
 import re
 import stanza
 
-def read_pdf(directory: str) -> tuple[str, int]:
-    '''
-    Reads all pdf files inside directory using pdfplumber and globing all pdf
-    with glob
-    And returns the text content that it could grab from all pdfs as a single
-    concatenated string and the number of pdf documents it found to be used to
-    find the IDF later (IDF = log(qtd de documentos / (DF + 1)))
-    Arg
-        directory (str): the name of the directory that you want to glob for
-        pdf files
-    '''
-    extracted_text = ''
+def glob_pdfs(directory: str) -> list[str]:
 
     if not directory.endswith('/'): 
         # In case the directory string doesn't ends with a slash, adds one
@@ -25,35 +14,56 @@ def read_pdf(directory: str) -> tuple[str, int]:
     if len(pdfs) == 0:
         raise LookupError('directory (str) must contain all'
         ' the pdf files you want to grab the text from.')
+    return pdfs
+
+def read_pdf(directory: str='input') -> tuple[str, int]:
+    '''
+    Reads all pdf files inside directory using pdfplumber and globing all pdf
+    with glob
+    And returns the text content that it could grab from all pdfs as a single
+    concatenated string and the number of pdf documents it found to be used to
+    find the IDF later (IDF = log(qtd de documentos / (DF + 1)))
+    Arg
+        directory (str): the name of the directory that you want to glob for
+        pdf files
+    '''
+    extracted_text: str = ''
+    pdfs: list[str] = glob_pdfs(directory)
 
     for pdf in pdfs:
+        # Reads pdf text content with pdfplumber and cats to extracted_text
         with pdfplumber.open(pdf) as fh:
-            # Reads pdf text content with pdfplumber and cats to extracted_text
             for i in range(len(fh.pages)):
                 extracted_text += fh.pages[i].extract_text()
+
     return extracted_text, len(pdfs)
 
-def clean_up(to_tokenize: str, 
-             to_sub: dict[str, str],
+def pretokenize(to_tokenize: str, 
+             to_sub: dict[str, str]={},
              stopwords: str='resources/stopwords.txt') -> list[list[str]]:
     '''
 
     '''
     stop_words = [word.strip() for word in open(stopwords).readlines()]
 
-    if to_sub:
+    if to_sub: 
+    # If the dictionary with regexes was passed, loops all keys and do re.sub
         for key in to_sub:
             to_tokenize = re.sub(key, to_sub[key], to_tokenize)
 
-    sntnc_tokens = [[t.lower() for t in sentence.split() 
-                      if t.lower() not in stop_words] 
-                    for sentence in to_tokenize.split('.') 
-                    if len(sentence)>1
-                   ]
-    return sntnc_tokens
+    sntnces: list[list[str]] = [[t.lower() for t in sentence.split()
+                                 if t.lower() not in stop_words]
+                                 for sentence in to_tokenize.split('.')
+                                 if len(sentence)>1 
+                                 # This if avoids empty lists in the result
+                               ]
+    return sntnces
 
 def lemmanize(sntnc_tokens: list[list[str]], 
               models_dir='resources/stanza_models/') -> list[str]:
+    '''
+
+    '''
     nlp = stanza.Pipeline(lang='pt', processors='tokenize,lemma', 
                           dir=models_dir,
                           tokenize_pretokenized=True)
@@ -63,7 +73,7 @@ def lemmanize(sntnc_tokens: list[list[str]],
 
 if __name__ == '__main__':
 
-    extracted_text, number_of_documents = read_pdf('input')
+    extracted_text, number_of_documents = read_pdf()
 
     to_sub = {
             # Essas duas primeiras chaves são para remover todas os números de
@@ -83,7 +93,7 @@ if __name__ == '__main__':
             r'\/| - ':'  ',
             }
 
-    sentences_tokenized = clean_up(extracted_text, to_sub)
+    sentences_tokenized = pretokenize(extracted_text, to_sub)
 
     lemmas = lemmanize(sentences_tokenized)
 
