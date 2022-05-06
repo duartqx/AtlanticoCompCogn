76042d1486b3ab963b1e414d6b7f0ad58b664677
Diego Duarte <duartqx@gmail.com>
2022-05-06 19:21:33 -0300

    moved grab_lemmas to it's own module and added get_neighbors function to main.py (for now)

--- projetos/projeto_nlp_1/main.py
+++ projetos/projeto_nlp_1/main.py
@@ -1,32 +1,23 @@
-from utils.clean_up import clean_up, flatten
-from utils.dataframe import NLPDataFrame
-from utils.lemmanizer import lemmanize
-from utils.read_pdf import glob_pdfs, read_pdf
+from utils.dataframe import DataFrame, NLPDataFrame
+from utils.grab_lemmas import grab_lemmas
 
-def grab_lemmas() -> list[list[str]]:
-    ''' globs for the pdfs, scrapes the text out of them, cleans up and
-    lemmanizes all strings '''
-    to_sub: dict[str, str] = {
-        # O conjunto chave e valor neste dicionário serão usados com re.sub
-        # para limpar os textos de números romanos, -se, tração, vs e
-        # transformando o nome de alguns locais em sigla para não se
-        # separarem na hora que a string for separada em tokens
-        r'I\.|I+ |IV| V |\-se|\—| vs|\d+': ' ',
-        'Rio de Janeiro': 'RJ', 
-        'São Paulo': 'SP', 
-        'Reino Unido': 'UK',
-        }
-    # Mounts the list of pdfs it can find on the corpus folder
-    pdfs: list[str] = glob_pdfs(directory='corpus')
-    # Reads all pdfs and scrapes it's text
-    scraped_text: list[str] = [read_pdf(pdf) for pdf in pdfs]
-    # Cleans up the text with re.sub and str.translate to remove stopwords,
-    # punctuation and everything in to_sub
-    clean_texts: list[str] = [clean_up(text, to_sub) for text in scraped_text]
-    # Lemmanizes the text with the module stanza
-    lemmas: list[list[str]] = [lemmanize(t) for t in clean_texts]
-
-    return lemmas
+def get_neighbors(flat_lemmas: list[str], to_find: str) -> list[str]:
+    ''' Returns a list of neighbors of to_find in tokens '''
+    flat_lemmas_len: int = len(flat_lemmas)
+    neighbors: list[str] = []
+    start: int = 0
+    while True:
+        try:
+            index: int = flat_lemmas.index(to_find, start)
+            # Only tries to append index+1 if it's smaller than
+            # flat_lemmas_len-1
+            if index + 1 < flat_lemmas_len-1:
+                neighbors.append(flat_lemmas[index+1])
+            neighbors.append(flat_lemmas[index-1])
+            start = (index + 1) 
+        except ValueError:
+            break
+    return neighbors
 
 def main(to_csv: bool=False) -> None:
     ''' The main function of the project, first executes grab_lemmas that
@@ -35,12 +26,10 @@ def main(to_csv: bool=False) -> None:
     Arg:
         to_csv (bool): If set to True, saves the full dataframe to a csv file
     '''
-
     df = NLPDataFrame(grab_lemmas())
     # Builds the dataframe using the lemmas from grab_lemmas and saves them in
     # the 'tokens' column and use them as base to calculate the other columns.
     # columns: ['tokens','tf','tf_mean','df','idf','tf_idfs','tf_idf_mean']
-
     if to_csv:
         df.to_csv('dataframe.csv', index=False)
 
@@ -50,7 +39,11 @@ def main(to_csv: bool=False) -> None:
     fl_list = five_largest_tf_idf['tokens'].tolist()
     # ['cirurgia', 'paciente', 'pequeno', 'dado', 'realizar']
 
-    print(five_largest_tf_idf)
+    # print(five_largest_tf_idf)
+    neighbors: dict[str, list[int]] = {t: get_neighbors(df.flat_lemmas, t) 
+                                       for t in fl_list}
+
+    print(neighbors)
 
     # Check on lemmas the closest words to fl_list
     # TODO
