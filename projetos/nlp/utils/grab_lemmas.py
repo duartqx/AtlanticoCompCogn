a276fe509cb192574012724491d15fde9a4048ef
Diego Duarte <duartqx@gmail.com>
2022-05-08 18:21:19 -0300

    merged grab_lemmas.py and lemmanizer.py

--- projetos/nlp/utils/grab_lemmas.py
+++ projetos/nlp/utils/grab_lemmas.py
@@ -1,7 +1,29 @@
-from .lemmanizer import lemmanize
 from .read_pdf import glob_pdfs, read_pdf
-from string import punctuation
+from os import path
 from re import sub
+from string import punctuation
+from typing import TypeAlias
+import stanza
+
+StanzaPipeline: TypeAlias = stanza.pipeline.core.Pipeline
+StanzaDoc: TypeAlias = stanza.models.common.doc.Document
+
+def lemmanize(to_lemanize: str, models_dir: str) -> list[str]:
+    '''
+    Returns a list of lemmanized words
+    Args:
+        sntnc_tokens (str): a string with pre_cleaned words
+        that will be lemmanized by stanza.Pipeline
+        models_dir (str): the path location to the stanza_models that you want
+        to apply
+
+    '''
+    nlp: StanzaPipeline = stanza.Pipeline(lang='pt', 
+                                          processors='tokenize,lemma', 
+                                          dir=models_dir, 
+                                          verbose=False)
+    doc: StanzaDoc = nlp(to_lemanize)
+    return [word.lemma for sent in doc.sentences for word in sent.words]
 
 def clean_up(to_clean: str, stopwords: str, to_sub: dict[str, str]={}) -> str:
     '''
