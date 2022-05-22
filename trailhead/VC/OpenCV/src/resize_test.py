f18941081285fa5c67efb649a2c32313f3c75187
Diego Duarte <duartqx@gmail.com>
2022-05-21 22:32:19 -0300

    removed test files

--- trailhead/VC/OpenCV/src/resize_test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-
-img = cv2.imread('../resources/ponte.jpg')
-h: int; w: int
-h, w, _ = img.shape
-print(h, w)
-print(type(h), type(w))
--- trailhead/VC/OpenCV/src/rot_test.py
+++
@@ -1,7 +0,0 @@
-import cv2
-img = cv2.imread('../resources/ponte.jpg')
-h, w = img.shape[:2]
-center = (w//2, h//2)
-M = cv2.getRotationMatrix2D(center, 30, 1.0)
-print(type(M), type(M[0]), type(M[0][0]))
-# <class 'numpy.ndarray'> <class 'numpy.ndarray'> <class 'numpy.float64'>
