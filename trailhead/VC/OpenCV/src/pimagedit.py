7cc5fa2e41c601d675cdf7e5f13daa3c49a0f1e8
Diego Duarte <duartqx@gmail.com>
2022-05-22 15:45:34 -0300

    continued working through the module adding more methods to my class

--- trailhead/VC/OpenCV/src/pimagedit.py
+++ trailhead/VC/OpenCV/src/pimagedit.py
@@ -1,8 +1,8 @@
-from __future__ import annotations
-from numpy import ndarray, uint8
+from numpy import ndarray, uint8, zeros
 from queue import Queue
-from typing import Optional, TypeAlias
+from typing import Optional, TypeAlias, Union
 import cv2
+import matplotlib.pyplot as plt
 
 BGRTuple: TypeAlias = tuple[uint8, uint8, uint8]
 CVImage: TypeAlias = 'ndarray[ndarray[ndarray[uint8]]]'
@@ -31,11 +31,18 @@ class ImageEditor:
         Editing {self.img}
         You have {self.edited_imgs.qsize()} unsaved edited images'''
 
-    def _put_last(self, edted_img: CVImage) -> None:
+    def _put_last(self, *args, operation: str='') -> None:
         ''' Adds edted_img to self.edited_imgs Queue and sets
         self.last_edited to edted_img '''
-        self.edited_imgs.put(edted_img)
-        self.last_edited = edted_img
+        for edted_img in args:
+            self.edited_imgs.put(edted_img)
+            self.last_edited = edted_img
+        if self.verb:
+            print(f'{operation} image file and added it to the save Queue')
+
+    @staticmethod
+    def _savefig(flname: str, fig: CVImage) -> None:
+        cv2.imwrite(flname, fig)
 
     def savefig(self, location: str=None) -> None:
         ''' Saves the edited image to an image file '''
@@ -47,8 +54,8 @@ class ImageEditor:
             location = ''
 
         for i in range(self.edited_imgs.qsize()):
-            filename: str = f'{location}{self.img_name}_ed_{i+1}.{self.img_ext}'
-            cv2.imwrite(filename, self.edited_imgs.get())
+            flname: str = f'{location}{self.img_name}_ed_{i+1}.{self.img_ext}'
+            self._savefig(flname, self.edited_imgs.get())
         if self.verb:
             print('Saved all edited images')
 
@@ -67,10 +74,7 @@ class ImageEditor:
                     edted_img[y, x] = color
                 elif brush == 'squares':
                     edted_img[y:y+5, x:x+5] = color
-        self._put_last(edted_img)
-        if self.verb:
-            print('Painted the image file and added it to the save Queue')
-        return edted_img
+        self._put_last(edted_img, operation='Painted')
 
     def paint_all(self, color: BGRTuple) -> CVImage:
         ''' Completelly paints the image with one rgb color (bgr_color) '''
@@ -88,9 +92,7 @@ class ImageEditor:
             size (tuple[int, int]) tuple with the new size for the image
         '''
         cropped_img: CVImage = self.img[size[0]:size[1], size[0]:size[1]]
-        self._put_last(cropped_img)
-        if self.verb:
-            print('Cropped the image file and added it to the save Queue')
+        self._put_last(cropped_img, operation='Cropped')
         return cropped_img
 
     def resize(self, new_width: int) -> CVImage:
@@ -104,9 +106,7 @@ class ImageEditor:
         resized_img: CVImage = cv2.resize(self.img.copy(), 
                                           new_size, 
                                           interpolation=cv2.INTER_AREA)
-        self._put_last(resized_img)
-        if self.verb:
-            print('Resized the image file and added it to the save Queue')
+        self._put_last(resized_img, operation='Resized')
         return resized_img
 
     def flip(self, axis: Union[str, int]) -> CVImage:
@@ -130,9 +130,7 @@ class ImageEditor:
             vertically and horizontally
             ''')
         flipped_img: CVImage = cv2.flip(self.img.copy(), ax)
-        self._put_last(flipped_img)
-        if self.verb:
-            print('Flipped the image file and added it to the save Queue')
+        self._put_last(flipped_img, operation='Flipped')
         return flipped_img
 
     def rotate(self, d: int) -> CVImage:
@@ -142,10 +140,110 @@ class ImageEditor:
         center: tuple[int, int] = (w // 2, h // 2) # Finds the center
         rot_matrix: RotationMatrix = cv2.getRotationMatrix2D(center, d, 1.0)
         rotated_img: CVImage = cv2.warpAffine(self.img, rot_matrix, (w, h))
-        self._put_last(rotated_img)
-        if self.verb:
-            print('Rotated the image file and added it to the save Queue')
+        self._put_last(rotated_img, operation='Rotated')
         return rotated_img
+
+    def colormode(self, color_format: str) -> CVImage:
+        ''' Alters self.img from bgr color format to gayscale, hsv or lab 
+        Arg:
+            color_format (str) Can be set to:
+                'gray' to convert to a grayscale version of self.img
+                'hsv' to convert to HSV version
+                'lab' to converto to lab color format
+        '''
+        altered_img: CVImage = self.img.copy()
+        if color_format == 'gray':
+            altered_img = cv2.cvtColor(altered_img, cv2.COLOR_BGR2GRAY)
+        elif color_format == 'hsv':
+            altered_img = cv2.cvtColor(altered_img, cv2.COLOR_BGR2HSV)
+        elif color_format == 'lab':
+            altered_img = cv2.cvtColor(altered_img, cv2.COLOR_BGR2LAB)
+        self._put_last(altered_img, operation='Altered color mode from')
+        return altered_img
+
+    def split(self, *args) -> tuple[CVImage, CVImage, CVImage]:
+        ''' Splits self.img color channels and adds them to the save Queue'''
+        blue_ch: CVImage; green_ch: CVImage; red_ch: CVImage;
+        blue_ch, green_ch, red_ch = cv2.split(self.img)
+        if not args:
+            self._put_last(blue_ch, green_ch, red_ch, operation='Splitted')
+        return blue_ch, green_ch, red_ch
+
+    @staticmethod
+    def merge(b_ch: CVImage, 
+              g_ch: CVImage, 
+              r_ch: CVImage, 
+              savefig=False) -> CVImage:
+        ''' Merges three separated color channels into a single image file '''
+        merged_chs: CVImage = cv2.merge([b_ch, g_ch, r_ch])
+        if savefig:
+            ImageEditor._savefig('merged_img.png', merged_chs)
+        return merged_chs
+
+    @staticmethod
+    def merge_single_channel(color_ch: CVImage, 
+                             color: str, 
+                             savefig: bool=False) -> CVImage:
+        zeros_ch: CVImage = zeros(color_ch.shape[:2], dtype='uint8')
+        merged_img: CVImage
+        if color == 'red':
+            merged_img = cv2.merge([zeros_ch, zeros_ch, color_ch])
+        elif color == 'green':
+            merged_img = cv2.merge([zeros_ch, color_ch, zeros_ch])
+        elif color == 'blue':
+            merged_img = cv2.merge([color_ch, zeros_ch, zeros_ch])
+        if savefig:
+            ImageEditor._savefig('merged_single_channel.png', merged_img)
+        return merged_img
+
+    @staticmethod
+    def plt_config(colormode: str, color_channels: tuple[CVImage]) -> None:
+        plt.figure()
+        plt.title(f'Histogram {colormode}')
+        plt.xlabel('Intensity')
+        plt.ylabel('Qtty of Pixels')
+        colors: list[str]
+        if len(color_channels) == 1: colors = ['gray']
+        else: colors = ['b','g','r']
+        for c_ch, color in zip(color_channels, colors):
+            hist: 'ndarray[float32]'= ImageEditor.calc_hist(c_ch)
+            plt.plot(hist, color=color)
+            plt.xlim([0, 256])
+
+
+    @staticmethod
+    def calc_hist(img: CVImage) -> 'ndarray[float32]':
+        ''' Returns the np array with the img histogram '''
+        return cv2.calcHist([img], [0], None, [256], [0, 256])
+
+    def plt_histogram(self, **kwargs) -> None:
+        '''
+        Plots self.img histogram
+        **kwargs:
+            ravel: bool = False
+            color: bool = False
+            saveplt: bool = False
+        '''
+        if 'color' not in kwargs:
+            # Plot the single B&W histogram
+            g_img: tuple[CVImage]
+            g_img = (cv2.cvtColor(self.img.copy(), cv2.COLOR_BGR2GRAY),)
+            if 'ravel' in kwargs:
+                # Plots ravel histogram
+                plt.hist(g_img[0].ravel(), 256, [0, 256])
+            else:
+                # Plots line histogram for b&w image
+                self.plt_config('B&W', g_img) 
+        else:
+            # Plots three histogram for each color channel
+            c_chs: tuple[CVImage, CVImage, CVImage] = self.split(0)
+            self.plt_config('Three color channels', c_chs)
+        if 'saveplt' in kwargs:
+            plt.savefig('plot.png')
+        plt.show()
+
+
+
 
 
 
@@ -159,4 +257,4 @@ if __name__ == '__main__':
     imgedit.flip('h')
     imgedit.rotate(30)
     imgedit.savefig(location='../exports')
-
+    imgedit.plt_histogram(saveplt=True, color=True)
