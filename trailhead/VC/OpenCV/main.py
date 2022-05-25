4ee915e1ea347db4819916f1b524207c4e89cf3b
Diego Duarte <duartqx@gmail.com>
2022-05-24 21:14:02 -0300

    Changed class name to Simp and continued working on adding methods to it (almost finishing adding methods since its only missing to implement identify and its staticmethods if any)

--- trailhead/VC/OpenCV/main.py
+++ trailhead/VC/OpenCV/main.py
@@ -1,11 +1,11 @@
-from src.pimagedit import ImageEditor
+from src.pimagedit import Simp
 
 def main() -> None:
 
-    ''' This main functions execute tests of some methods in the ImageEditor
+    ''' This main functions execute tests of some methods in the SIMP
     class. Still incomplete since the class is still a wip '''
 
-    imgedit = ImageEditor(img='resources/ponte.jpg', savelocation='exports')
+    imgedit = Simp(img='resources/ponte.jpg', savelocation='exports')
     imgedit.crop((100, 200))
     imgedit.paint_all((0,0,255))
     imgedit.paint_squares((0,0,255))
@@ -14,9 +14,16 @@ def main() -> None:
     imgedit.median_blur_montage()
     imgedit.mean_blur_montage()
     imgedit.bilateral_filter_montage()
-    #imgedit.plt_histogram(equalize=True, saveplt=True)
     imgedit.equalize()
+    imgedit.normal_binary_threshold_montage()
+    imgedit.adaptive_binary_threshold_montage()
+    imgedit.mahotas_montage()
+    imgedit.sobel()
+    imgedit.laplace()
     imgedit.savefigs()
+
+    identifying = Simp(img='resources/dados.png', savelocation='exports')
+    identifying.identify()
 
 if __name__ == '__main__':
 
--- trailhead/VC/OpenCV/src/pimagedit.py
+++ trailhead/VC/OpenCV/src/pimagedit.py
@@ -2,15 +2,18 @@ from queue import Queue
 from typing import Callable, TypeAlias, Union
 from os import path
 import cv2
+import mahotas as mh
 import matplotlib.pyplot as plt
 import numpy as np
 
 BGRTuple: TypeAlias = tuple[np.uint8, np.uint8, np.uint8]
-CVImage: TypeAlias = 'np.ndarray[np.ndarray[np.ndarray[np.uint8]]]'
+CVColorImage: TypeAlias = 'np.ndarray[np.ndarray[np.ndarray[np.uint8]]]'
+CVBwImage: TypeAlias = 'np.ndarray[np.ndarray[np.uint8]]'
+CVImage: TypeAlias = Union[CVBwImage, CVColorImage]
 RotationMatrix: TypeAlias = 'np.ndarray[np.ndarray[np.float64]]'
 
-class ImageEditor:
-    ''' Python extremely simple image editor using OpenCV '''
+class Simp:
+    ''' Simple Image Manipulator in Python (SIMP) using OpenCV '''
     def __init__(self, img: str, savelocation: str, verb: bool=False) -> None:
         # Save location for edited images and Plots
         self.savelocation = savelocation
@@ -45,13 +48,13 @@ class ImageEditor:
 
     def _get_filename(self, i: int, operation: str) -> str:
         ''' Builds the filename string to be used with savefig '''
-        return f'{self.name_n_ext[0]}_{operation}_ed_{i}{self.name_n_ext[1]}'
+        return f'{self.name_n_ext[0]}_ed_{i}_{operation}{self.name_n_ext[1]}'
 
     def savefigs(self) -> None:
         ''' Saves the edited image to an image file '''
         for i in range(1, self.edited_imgs.qsize()+1):
             img_tup: tuple[str, CVImage] = self.edited_imgs.get()
-            fn: str = self._get_filename(i, img_tup[0])
+            fn: str = self._get_filename(i, operation=img_tup[0])
             flname_path: str = path.join(self.savelocation, fn)
             self._savefig(flname_path, img_tup[1])
 
@@ -161,30 +164,31 @@ class ImageEditor:
         self._put_last(altered_img, operation='Altered_color_mode_from')
         return altered_img
 
-    def split(self, *args) -> tuple[CVImage, CVImage, CVImage]:
+    def split(self, *args) -> tuple[CVBwImage, CVBwImage, CVBwImage]:
         ''' Splits self.img color channels and adds them to the save Queue'''
         blue_ch: CVImage; green_ch: CVImage; red_ch: CVImage;
         blue_ch, green_ch, red_ch = cv2.split(self.img)
+        print(type(blue_ch), type(blue_ch[0]), type(blue_ch[0][0]))
         if not args:
             self._put_last(blue_ch, green_ch, red_ch, operation='Splitted')
         return blue_ch, green_ch, red_ch
 
     @staticmethod
-    def merge(b_ch: CVImage, 
-              g_ch: CVImage, 
-              r_ch: CVImage, 
+    def merge(b_ch: CVBwImage, 
+              g_ch: CVBwImage, 
+              r_ch: CVBwImage, 
               savefig=False) -> CVImage:
         ''' Merges three separated color channels into a single image file '''
         merged_chs: CVImage = cv2.merge([b_ch, g_ch, r_ch])
         if savefig:
-            ImageEditor._savefig('merged_img.png', merged_chs)
+            ImageManipulatorInPython._savefig('merged_img.png', merged_chs)
         return merged_chs
 
     @staticmethod
-    def merge_single_channel(color_ch: CVImage, 
+    def merge_single_channel(color_ch: CVBwImage, 
                              color: str, 
                              savefig: bool=False) -> CVImage:
-        zeros_ch: CVImage = np.zeros(color_ch.shape[:2], dtype='uint8')
+        zeros_ch: CVBwImage = np.zeros(color_ch.shape[:2], dtype='uint8')
         merged_img: CVImage
         if color == 'red':
             merged_img = cv2.merge([zeros_ch, zeros_ch, color_ch])
@@ -193,20 +197,20 @@ class ImageEditor:
         elif color == 'blue':
             merged_img = cv2.merge([color_ch, zeros_ch, zeros_ch])
         if savefig:
-            ImageEditor._savefig('merged_single_channel.png', merged_img)
+            ImageManipulatorInPython._savefig('merged_single_channel.png', merged_img)
         return merged_img
 
     @staticmethod
-    def plt_config(method: str, color_channels: tuple[CVImage]) -> None:
+    def plt_config(method: str, c_chnls: tuple[CVImage]) -> None:
         plt.figure()
         plt.title(f'Histogram {method}')
         plt.xlabel('Intensity')
         plt.ylabel('Qtty of Pixels')
         colors: list[str]
-        if len(color_channels) == 1: colors = ['gray']
+        if len(c_chnls) == 1: colors = ['gray']
         else: colors = ['b','g','r']
-        for c_ch, color in zip(color_channels, colors):
-            hist: 'ndarray[float32]'= ImageEditor.calc_hist(c_ch)
+        for c_ch, color in zip(c_chnls, colors):
+            hist: 'ndarray[float32]'= ImageManipulatorInPython.calc_hist(c_ch)
             plt.plot(hist, color=color)
             plt.xlim([0, 256])
 
@@ -215,21 +219,30 @@ class ImageEditor:
         ''' Returns the np array with the img histogram '''
         return cv2.calcHist([img], [0], None, [256], [0, 256])
 
-    @staticmethod
-    def _get_bw(img: CVImage) -> CVImage:
-        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def grayscale(self, q: bool=True) -> CVBwImage:
+        ''' Converts and returns self.img as a black and white image 
+        Arg
+            q (bool) if True saves grayscale to the Queue
+        '''
+        bw: CVBwImage = self._grayscale()
+        self._put_last(bw, operation='Grayscaled')
+        return bw
 
-    def equalize(self, img: CVImage=None) -> CVImage:
+    def _grayscale(self) -> CVBwImage:
+        ''' Converts and returns self.img to grayscale '''
+        return cv2.cvtColor(self.img.copy(), cv2.COLOR_BGR2GRAY)
+
+    def equalize(self, img: CVBwImage=None) -> CVImage:
         ''' Equalizes img's histogram adds it to the save Queue and returns the
         img so that it's histogram can be plotted '''
         if img is None:
-            img = self._get_bw(self.img.copy())
+            img = self._grayscale()
         eq_img: CVImage = cv2.equalizeHist(img)
         self._put_last(eq_img, operation='Equalized')
         return eq_img
 
     @staticmethod
-    def _ravel(g_img: CVImage) -> None:
+    def _ravel(g_img: CVBwImage) -> None:
         ''' Plots ravel histogram '''
         plt.hist(g_img.ravel(), 256, [0, 256])
 
@@ -248,8 +261,8 @@ class ImageEditor:
             self.plt_config('Three color channels', c_chs)
         else:
             # Plot the single B&W histogram
-            g_img: tuple[CVImage]
-            g_img = self._get_bw(self.img.copy())
+            g_img: CVBwImage
+            g_img = self._grayscale()
             if kwargs.get('equalize', None) is not None:
                 g_img = self.equalize(g_img); self._ravel(g_img)
             elif kwargs.get('ravel', None) is not None:
@@ -262,6 +275,16 @@ class ImageEditor:
             plt.savefig(path.join(self.savelocation, 'plot.png'))
         plt.show()
 
+    @staticmethod
+    def _gaussian(img: CVImage, amount: int) -> CVImage:
+        ''' Applies gaussian blur to self.img '''
+        return cv2.GaussianBlur(img, (amount, amount), 0)
+
+    def gaussian(self, amount: int) -> CVImage:
+        blurred_img: CVImage = self._gaussian(amount)
+        self._put_last(blurred_img, operation='Gaussian_blurred')
+        return blurred_img
+
     def _blur_montage(self, func: Callable, oper: str, **kwargs) -> CVImage:
         '''
         Create Blurs montage of self.img with cv2.medianBlur, cv2.blur or
@@ -270,11 +293,11 @@ class ImageEditor:
             func (Callable) Either cv2.medianBlur or cv2.blur
             oper (str) either 'Median Blurred' or 'Mean Blurred'
             kwargs
-                kwargs['3'] (int | tuple[int]): 3, (3, 3) or (3, 21, 21)
-                kwargs['5'] (int | tuple[int]): 5, (5, 5) or (5, 35, 35)
-                kwargs['7'] (int | tuple[int]): 7, (7, 7) or (7, 49, 49)
-                kwargs['9'] (int | tuple[int]): 9, (9, 9) or (9, 63, 63)
-                kwargs['11'] (int | tuple[int]): 11, (11, 11) or (11, 77, 77)
+                kwargs['3'] (int | tuple[int]): (3,), (3, 3) or (3, 21, 21)
+                kwargs['5'] (int | tuple[int]): (5,), (5, 5) or (5, 35, 35)
+                kwargs['7'] (int | tuple[int]): (7,), (7, 7) or (7, 49, 49)
+                kwargs['9'] (int | tuple[int]): (9,), (9, 9) or (9, 63, 63)
+                kwargs['11'] (int | tuple[int]): (11,), (11, 11) or (11, 77, 77)
         '''
         img: CVImage = self.img.copy()[::2, ::2]
         edted_img: CVImage = np.vstack([
@@ -301,3 +324,134 @@ class ImageEditor:
         kwargs: dict[str, tuple[int, int, int]]
         kwargs = {str(i): (i, i*7, i*7) for i in range(3, 12, 2)}
         return self._blur_montage(cv2.bilateralFilter, 'Bilateral', **kwargs)
+
+    def _four_montage(self, *args) -> CVImage: 
+        '''
+        Returns a montage of four images
+        args:
+            img_1: CVImage
+            img_2: CVImage
+            img_3: CVImage
+            img_4: CVImage
+        '''
+        first_row: CVImage = np.hstack([args[0], args[1]])
+        second_row: CVImage = np.hstack([args[2], args[3]])
+        return np.vstack([first_row, second_row]) 
+
+    @staticmethod
+    def _get_both_threshold(img: CVBwImage) -> tuple[CVBwImage, CVBwImage]:
+        T: float; bin: CVImage; binI: CVImage
+        T, bin = cv2.threshold(img, 160, 255, cv2.THRESH_BINARY)
+        T, binI = cv2.threshold(img, 160, 255, cv2.THRESH_BINARY_INV)
+        return bin, binI
+
+    @staticmethod
+    def _get_adap(img: CVBwImage, method: str) -> CVBwImage:
+        ''' Returns a cv2.adaptiveThreshold filtered CVImage '''
+        bin: int; adap: int
+        bin = cv2.THRESH_BINARY_INV
+        if method == 'mean':
+            adap = cv2.ADAPTIVE_THRESH_MEAN_C
+        elif method == 'gaussian':
+            adap = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
+        return cv2.adaptiveThreshold(img, 255, adap, bin, 21, 5)
+
+    def _get_bth_adp_threshold(self, 
+                               img: CVBwImage) -> tuple[CVBwImage, CVBwImage]:
+        bin_mean: CVBwImage; bin_gaussian: CVBwImage
+        bin_mean = self._get_adap(img, method='mean')
+        bin_gaussian = self._get_adap(img, method='gaussian')
+        return bin_mean, bin_gaussian
+
+    def _threshold_montage(self, adap: bool=False) -> CVBwImage:
+        ''' Returns a montage with four binary threshold edits 
+        normal threshold if adap = False and adaptiveThreshold if adap = True
+        '''
+        img: CVBwImage; blurred: CVBwImage; 
+        img = self._grayscale()
+        blurred = self._gaussian(img, amount=7)
+
+        bin_a: CVBwImage; bin_b: CVBwImage; 
+
+        if adap:
+            bin_a, bin_b = self._get_bth_adp_threshold(blurred)
+        else:
+            bin_a, bin_b = self._get_both_threshold(blurred)
+            #bitand: CVImage = cv2.bitwise_and(img, img, mask=bin_b)
+        # Get the montage
+        bin_montage: CVImage
+        bin_montage = self._four_montage(img, blurred, bin_a, bin_b)
+        self._put_last(bin_montage)
+        return bin_montage
+
+    def normal_binary_threshold_montage(self) -> CVBwImage:
+        ''' Normal binary threshold filter montage '''
+        return self._threshold_montage()
+
+    def adaptive_binary_threshold_montage(self) -> CVBwImage:
+        ''' Adaptive binary threshold filter montage '''
+        return self._threshold_montage(adap=True)
+
+    def _get_normalized(self, T) -> CVImage:
+        ''' Normalizes image '''
+        temp: CVBwImage = self._grayscale()
+        temp[temp > T] = 255
+        temp[temp < 255] = 0
+        return cv2.bitwise_not(temp)
+
+    def _get_mahota(self, model: Callable, blurred: CVBwImage) -> CVBwImage:
+        ''' Returns a normalized mahota otsu or rc 
+        Arg:
+            model (Callable): Can be either mh.thresholding.otsu or
+            mh.thresholding.rc
+            blurred (CVBwImage): Black and white self.img with gaussian applied
+        '''
+        return self._get_normalized(model(blurred))
+
+    def mahotas_montage(self) -> CVBwImage:
+        bw_img: CVBwImage; blrrd: CVBwImage; otsu: CVBwImage; rc: CVBwImage
+        bw_img = self._grayscale()
+        blrrd = self._gaussian(bw_img, 7)
+        otsu = self._get_mahota(mh.thresholding.otsu, blrrd)
+        rc = self._get_mahota(mh.thresholding.rc, blrrd)
+        mh_montage: CVBwImage = self._four_montage(bw_img, blrrd, otsu, rc)
+        self._put_last(mh_montage, operation='Otsu_RC_Threshold')
+        return mh_montage
+
+    @staticmethod
+    def _get_sobel(img: CVImage, axis: tuple[int, int]) -> CVBwImage:
+        ''' Calculates and returns Sobel '''
+        return np.uint8(np.absolute(cv2.Sobel(img, cv2.CV_64F, *axis)))
+
+    def sobel(self) -> CVBwImage:
+        img: CVBwImage = self._grayscale()
+        sobel_x: CVBwImage = self._get_sobel(img, (1,0))
+        sobel_y: CVBwImage = self._get_sobel(img, (0,1))
+        sobel: CVBwImage = cv2.bitwise_or(sobel_x, sobel_y)
+        s_montage: CVBwImage = self._four_montage(img, sobel_x, sobel_y, sobel)
+        self._put_last(s_montage, operation='Sobel')
+        return s_montage
+
+    def laplace(self) -> CVBwImage:
+        img: CVBwImage = self._grayscale()
+        lap: CVBwImage = np.uint8(np.absolute(cv2.Laplacian(img, cv2.CV_64F)))
+        lap_montage: CVBwImage = np.vstack([img, lap])
+        self._put_last(lap_montage, operation='Laplace')
+        return lap_montage
+
+    @staticmethod
+    def _both_canny(blurred: CVBwImage) -> tuple[CVBwImage, CVBwImage]:
+        return cv2.Canny(blurred, 20, 120), cv2.Canny(blurred, 70, 200)
+
+    def canny(self) -> CVBwImage:
+        img: CVBwImage = self._grayscale()
+        blrrd: CVBwImage = self._gaussian(img, amount=7)
+        canny_1: CVBwImage; canny_2: CVBwImage; canny_montage: CVBwImage
+        canny_1, canny_2 = self._both_canny(blrrd)
+        canny_montage = self._four_montage(img, blrrd, canny_1, canny_2)
+        self._put_last(canny_montage, operation='Canny')
+        return canny_montage
+
+    def identify(self) -> CVImage:
+        pass
+
